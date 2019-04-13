51363d76eab701f867a5d285991b10764496b16f
Wyxwx <791989352@qq.com>
2019-04-13 18:45:01 +0800

    Eureka （二） 身份验证

---
+++ eureka_server/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.demo.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Auther: wyx
+ * @Date: 2019-04-13 17:12
+ * @Description:
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
